cd428a939373eb134ed2ae7bdcda0317de10d10f
smile <s930175@gmail.com>
2022-09-25 22:55:06 +0800

    雙螢幕喔喔喔

---
+++ test2/108命名空間.cs
@@ -0,0 +1,19 @@
+﻿//命名空間
+//類屬於命名空間
+//若項目中無此命名空間，需手動導入這個類所在的命名空間
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using 靜態與非靜態;
+//在一個項目中引用另一個項目的類
+//1)相依性 右鍵新增專案參考
+
+namespace test2
+{
+    class _108命名空間
+    {
+        //Student s1 = new Student();//靜態類不允許實例化
+        Person p = new Person();
+    }
+}
---
+++ test2/109值類型和引用類型.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test2
+{
+    class _109值類型和引用類型
+    {
+        //1.值類型和引用類型在內存尚存儲的
+    }
+}
