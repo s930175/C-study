896cc480481f75872a93df387561ad515dfd2c35
smile <s930175@gmail.com>
2022-09-26 23:15:51 +0800

    新增字串方法練習

--- test2/111字串方法.cs
+++ test2/111字串方法.cs
@@ -8,11 +8,12 @@ namespace test2
 {
     class _111字串方法
     {
+        //字串轉數組後改變其中一個字符的值
         public static void F()
         {
             //可將string 視為char的唯獨數組
             string s = "abcde";
-            //也可以通過index去訪問字串中的某個元素
+            //也可以通過index去訪問字串中的某個字符
             Console.WriteLine(s[0]);
             //s[0] = 'b';//不可
             //首先將字串轉為char類型的數組
@@ -21,6 +22,7 @@ namespace test2
             //將數組轉為字串
             s = new string(chs);
         }
+        //不佔內存的修改字串
         public void F2()
         {
             StringBuilder sb = new StringBuilder();//不會另外開闢內存空間
@@ -38,6 +40,7 @@ namespace test2
             Console.WriteLine(sw.Elapsed);
             Console.ReadKey();
         }
+        //字串比對(不分大小寫)
         public static void Practice()
         {
             string s = "123245678";
@@ -48,9 +51,22 @@ namespace test2
             //ABC = ABC.ToLower();//轉小寫
             if (abc.Equals(ABC,StringComparison.OrdinalIgnoreCase))
             {
-                Console.WriteLine("Equals(string,StringComparison.OrdinalIgnoreCase)可以忽略被比較的大小寫");
+                Console.WriteLine("StringComparison.OrdinalIgnoreCase可以忽略被比較的大小寫");
             }
         }
-        
+        //字串分割，移除不要的
+        public static void Practice2()
+        {
+            string s = "a b    def _  +    =  ,,, fffs";
+            char[] chs = { ' ', '_','+','=',',' };//想從字串移除的字符
+            string[] str = s.Split(chs,StringSplitOptions.RemoveEmptyEntries);
+            //StringSplitOptions.RemoveEmptyEntries把""移除
+        }
+        public static void Practice3()
+        {
+            string s = "2008-08-08";
+            //char[] chs = { '-' };
+            string[] date = s.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+        }
     }
 }
